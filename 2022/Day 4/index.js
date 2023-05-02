af348097d965d5eb6ef35725aa181937f04268ac
alyasovyan <alyasovyan@gmail.com>
2023-05-02 11:22:40 +0200

    Day 4 task 2 done

--- 2022/Day 4/index.js
+++ 2022/Day 4/index.js
@@ -5,25 +5,52 @@ dotenv.config();
 main();
 
 function main() {
-    let count = 0;
+    let firstTaskCount = 0;
+    let secondTaskCount = 0;
     const data = process.env.DATA;
     const arrayOfData = data.split('\n');
+
     for(const range of arrayOfData) {
         const arrayOfRange = range.split(',');
-        const firstRange = generateRangeString(arrayOfRange[0])
-        const secondRange = generateRangeString(arrayOfRange[1])
+        const firstRange = generateRangeString(arrayOfRange[0]);
+        const secondRange = generateRangeString(arrayOfRange[1]);
 
+        // part 1
         if(firstRange.includes(secondRange) || secondRange.includes(firstRange)) {
-            count += 1
+            firstTaskCount += 1
+        }
+
+        // part 2
+        const firstRangeArray = generateRangeArray(arrayOfRange[0]);
+        const secondRangeArray = generateRangeArray(arrayOfRange[1]);
+        for(const index in firstRangeArray) {
+            if(secondRangeArray.includes(firstRangeArray[index])) {
+                secondTaskCount += 1;
+                break;
+            }
         }
     }
-    console.log(count)
+    console.log(firstTaskCount, secondTaskCount)
 }
 
-function generateRangeString(range) {
+function generateRangeArray(range) {
+    const { startOfRange, endOfRange } = generateIndexes(range);
+    const array = [];
+    for(let i = startOfRange; i <= endOfRange; i++) {
+        array.push(i);
+    }
+    return array;
+}
+
+function generateIndexes(range) {
     const indexOfDash = range.indexOf('-');
     const startOfRange = Number(range.slice(0, indexOfDash));
     const endOfRange = Number(range.slice(indexOfDash + 1));
+    return { startOfRange, endOfRange }
+}
+
+function generateRangeString(range) {
+    const { startOfRange, endOfRange } = generateIndexes(range);
     let string = "";
     for(let i = startOfRange; i <= endOfRange; i++) {
         string += " " + i + ","
